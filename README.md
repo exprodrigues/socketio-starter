bae1ba0576051f74a8856d22b65b1cf89066d8a3
Sergio Rodrigues <sergio@devindex.com.br>
2017-02-23 14:47:37 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# socket.io starter
+
+Starter example using socket.io and VueJS
+
+## Setup and run
+
+```
+# install dependencies
+npm install
+
+# serve at localhost:1337
+node index.js
+```
---
+++ index.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const fs = require('fs');
+const socketio = require('socket.io');
+
+const server = http.createServer((req, res) => {
+	fs.readFile('./index.html', (error, content) => {
+		res.writeHead(200, { 'Content-Type': 'text/html' });
+		res.end(content, 'utf-8');
+	});
+});
+
+const io = socketio(server);
+
+io.on('connection', function(client) {
+	client.on('message', message => client.emit('message', message));
+
+	client.emit('time', Date.now());
+	setInterval(() => client.emit('time', Date.now()), 1000);
+	
+	client.on('disconnect', () => {});
+});
+
+server.listen(1337);
