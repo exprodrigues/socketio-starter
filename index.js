b7b0e8e65e3404ee196c89e77a49c6558cbfe7c5
Sergio Rodrigues <sergio@devindex.com.br>
2017-02-23 16:22:51 -0300

    Converting tabs to spaces

--- index.js
+++ index.js
@@ -3,21 +3,21 @@ const fs = require('fs');
 const socketio = require('socket.io');
 
 const server = http.createServer((req, res) => {
-	fs.readFile('./index.html', (error, content) => {
-		res.writeHead(200, { 'Content-Type': 'text/html' });
-		res.end(content, 'utf-8');
-	});
+    fs.readFile('./index.html', (error, content) => {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(content, 'utf-8');
+    });
 });
 
 const io = socketio(server);
 
 io.on('connection', function(client) {
-	client.on('message', message => client.emit('message', message));
+    client.on('message', message => client.emit('message', message));
 
-	client.emit('time', Date.now());
-	setInterval(() => client.emit('time', Date.now()), 1000);
-	
-	client.on('disconnect', () => {});
+    client.emit('time', Date.now());
+    setInterval(() => client.emit('time', Date.now()), 1000);
+    
+    client.on('disconnect', () => {});
 });
 
 server.listen(1337);
