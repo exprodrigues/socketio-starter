acf68b05bc04ea264f8d2ab56287afd409056020
Sergio Rodrigues <sergio@devindex.com.br>
2017-02-23 16:23:58 -0300

    Sending message to all clients

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const server = http.createServer((req, res) => {
 const io = socketio(server);
 
 io.on('connection', function(client) {
-    client.on('message', message => client.emit('message', message));
+    client.on('message', message => io.emit('message', message));
 
     client.emit('time', Date.now());
     setInterval(() => client.emit('time', Date.now()), 1000);
